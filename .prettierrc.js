9605eb07c306274c3ad2b1fc61a6bbb396ccdbd6
juan-calle <juancmcalle@gmail.com>
2021-06-27 05:06:44 +0100

    :rotating_light:  Install and configure code editting tools
    Install and configure eslint, prettier, lint-st

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: true,
+  tabWidth: 2,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  parser: 'typescript',
+  endOfLine: 'lf'
+}
