9ab9a4c3817838f88e747960eec9e913ecb0ff6d
William von Arend <willvonarend@gmail.com>
2021-06-28 19:29:12 +0200

    :sparkles: (⚛️ Frontend) add styles to receipt layout

--- client/src/containers/receipt/Receipt-Body/receipt-body.tsx
+++ client/src/containers/receipt/Receipt-Body/receipt-body.tsx
@@ -4,7 +4,29 @@ import React from 'react';
 function ReceiptBody(): JSX.Element {
 	return (
 		<div className="Receipt-Body">
-			<h5>Receipt Body</h5>
+			<div className="Company-Details">
+				<p className="Company-Name">Sainsbury&apos;s</p>
+				<p className="Company-Logo">Sainsbury&apos;s</p>
+			</div>
+			<div className="Time-Container">
+				<p className="Timestamp">12th May 2021 15:50pm</p>
+				<p className="Circle">🦞</p>
+			</div>
+			<div className="Item-List-Container">
+				<ul className="List">
+					<li className="List-item">Pizza 4 Stazzione</li>
+					<li className="List-item">Strongbow Cider</li>
+					<li className="List-item">Chicken breast fillets</li>
+					<li className="List-item">Ice cream</li>
+				</ul>
+			</div>
+			<div className="Total-Container">
+				<div className="Price-Container">
+					<p className="Total-Title">Total:</p>
+					<p className="Total-Price">€16.50</p>
+				</div>
+				<p className="Receipt-Id">Id: 2793927</p>
+			</div>
 		</div>
 	);
 }
