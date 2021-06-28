6fd89ebe8b7168694ec6770d4abcf9ca16d81210
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-28 17:09:00 +0200

    :wrench: (🐈 Backend) docker; users, stores and receipts resources; TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,14 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ReceiptsModule } from './receipts/receipts.module';
+import { UsersModule } from './users/users.module';
+import { StoresModule } from './stores/stores.module';
 import AppController from './app.controller';
 import AppService from './app.service';
 
-@Module({ imports: [], controllers: [AppController], providers: [AppService] })
+@Module({
+	imports: [TypeOrmModule.forRoot(), ReceiptsModule, UsersModule, StoresModule],
+	controllers: [AppController],
+	providers: [AppService],
+})
 export default class AppModule {}
