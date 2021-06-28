9ebe18ed4b7b4433e758e5e30282e733db5d86fd
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-28 20:09:37 +0200

    :card_file_box: (🐈 Backend) Add stores entity

--- server/src/stores/entities/store.entity.ts
+++ server/src/stores/entities/store.entity.ts
@@ -1,1 +1,22 @@
-export class Store {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Store {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	users: string;
+
+	@Column()
+	address: string;
+
+	@Column()
+	number: string;
+
+	@Column()
+	logo: string;
+
+	@Column()
+	category: string;
+}
