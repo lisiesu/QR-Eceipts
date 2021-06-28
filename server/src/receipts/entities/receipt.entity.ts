1ac3a8337c49a23204aaca762357d46198771ded
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-28 20:14:19 +0200

    :card_file_box: (🐈 Backend) Add receipt entity skeleton

--- server/src/receipts/entities/receipt.entity.ts
+++ server/src/receipts/entities/receipt.entity.ts
@@ -1,1 +1,13 @@
-export class Receipt {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Receipt {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ type: 'timestamp' })
+	timeOfPurchase: Date;
+
+	@Column({ type: 'jsonb' })
+	content: JSON;
+}
