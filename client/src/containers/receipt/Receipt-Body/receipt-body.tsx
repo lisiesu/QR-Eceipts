0ec9642299ad4e732894d266c6e7ca96a456c76b
William von Arend <willvonarend@gmail.com>
2021-06-28 15:19:51 +0200

    :sparkles: (⚛️ Frontend) add routing between receipt and receipt-list

---
+++ client/src/containers/receipt/Receipt-Body/receipt-body.tsx
@@ -0,0 +1,12 @@
+import './receipt-body.css';
+import React from 'react';
+
+function ReceiptBody(): JSX.Element {
+	return (
+		<div className="Receipt-Body">
+			<h5>Receipt Body</h5>
+		</div>
+	);
+}
+
+export default ReceiptBody;
---
+++ client/src/containers/receipt/header/header.tsx
@@ -0,0 +1,12 @@
+import './header.css';
+import React from 'react';
+
+function Header(): JSX.Element {
+	return (
+		<div className="Header">
+			<h3>Header</h3>
+		</div>
+	);
+}
+
+export default Header;
