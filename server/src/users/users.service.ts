adf217202191ef161aba11e48f64bd6930c0dd14
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-30 17:02:24 +0200

    :sparkles: (🐈 Backend) Add post /users

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,39 +1,54 @@
+import {
+	Injectable,
+	ConflictException,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import Hashids = require('hashids');
-import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserSchema } from './entities/user.entity';
 import User from './entities/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 const hashids = new Hashids();
-
-// import { CreateUserDto } from './dto/create-user.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
 	constructor(
 		@InjectRepository(UserSchema)
-		private usersRepository: Repository<User>,
+		private repository: Repository<User>,
 	) {}
 
-	create() {
-		return undefined;
+	async create(createUserDto: CreateUserDto) {
+		try {
+			const found = await this.repository.find({
+				where: { email: createUserDto.email },
+			});
+			if (found.length !== 0) throw new ConflictException(); // Without querying before the id counter increases even if TypeORM save fails.
+			const user = this.repository.create(createUserDto);
+			const userSaved = await this.repository.save(user);
+			return userSaved;
+		} catch (err) {
+			const { code, status } = err;
+			if (code === '23505' || status === 409) throw new ConflictException(); // code refers to TypeORM save() error and status for ConlictException error.
+			throw new InternalServerErrorException();
+		}
 	}
 
 	findAll() {
 		return hashids.encode(1234);
 	}
 
-	// findOne(id: number) {
-	// 	return `This action returns a #${id} user`;
-	// }
+	findOne(id: number) {
+		return `This action returns a #${id} user`;
+	}
 
-	// update(id: number, updateUserDto: UpdateUserDto) {
-	// 	return updateUserDto;
-	// }
+	update(id: number, updateUserDto: UpdateUserDto) {
+		return updateUserDto;
+	}
 
-	// remove(id: number) {
-	// 	return `This action removes a #${id} user`;
-	// }
+	remove(id: number) {
+		return `This action removes a #${id} user`;
+	}
 }
