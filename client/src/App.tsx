7a7ca57558ec7e5021079693c600c7d31337af8e
William <willvonarend@gmail.com>
2021-07-01 13:12:33 +0200

    :sparkles: (⚛️ Frontend) add routing between receipt and receipt-list route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Header from './containers/Receipt/Header/header';
+import Header from './components/Header/Header';
 import * as service from './services/ServerAPIServices';
 import Receipt from './interfaces/types';
 
@@ -14,19 +14,18 @@ function App(): JSX.Element {
 	}, []);
 
 	return (
-		<Router>
+		<BrowserRouter>
 			<div className="App">
 				<Switch>
 					<Route exact path="/receipt">
-						<Header headerReceipt={receipts} />
-						{console.log(receipts)}
+						<Header headerReceipt={receipts} title="Your Receipt!" />
 					</Route>
 					<Route exact path="/receipt-list">
-						<Header headerReceipt={receipts} />
+						<Header headerReceipt={receipts} title="Your Receipts List" />
 					</Route>
 				</Switch>
 			</div>
-		</Router>
+		</BrowserRouter>
 	);
 }
 
