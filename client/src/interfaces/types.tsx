becd192c4a9e92787c44db03cb7cd9ad0d14543d
William <willvonarend@gmail.com>
2021-07-01 16:46:15 +0200

    :label: (⚛️ Frontend) update types to match backend format

--- client/src/interfaces/types.tsx
+++ client/src/interfaces/types.tsx
@@ -1,19 +1,31 @@
 export default interface Receipt {
 	id: string;
-	timeOfPurchase: string;
-	products: Product[];
+	timeOfPurchase: Date;
+	products: ProductInterface[];
 	total: number;
 	currency: string;
 	paymentMethod: string;
-	cardNumber: number;
-	misc: string;
-	user: string;
-	store: string;
+	cardNumber: string;
+	misc?: string;
+	store: Store;
+	category: Category;
 }
-
-export interface Product {
+export interface ProductInterface {
+	id: string;
 	product: string;
 	price: number;
 	vat: number;
 	isRefunded: boolean;
+	refundDate?: Date;
+}
+export interface Store {
+	id: number;
+	name: string;
+	address: string;
+	logo: string;
+	storeNumber: string;
+}
+export interface Category {
+	id: number;
+	name: string;
 }
