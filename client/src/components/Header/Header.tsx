70265c4ed0f0c0b889c326d3eaa636541739eee6
juan-calle <juancmcalle@gmail.com>
2021-07-02 14:57:18 +0100

    :twisted_rightwards_arrows:  Merge with last changes in dev
    Merged with backend

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Header.css';
 
 import ReceiptBody from '../Receipt/ReceiptBody/ReceiptBody';
-import ReceiptList from '../Receipt/ReceiptList/ReceiptList';
-import Receipt from '../../interfaces/types';
 
 interface Props {
 	title: string;
@@ -13,11 +11,7 @@ function Header({ title }: Props): JSX.Element {
 	return (
 		<div className="Header">
 			<p className="title">{title}</p>
-			{title === 'Your Receipt!' ? (
-				<ReceiptBody receipt={headerReceipt} />
-			) : (
-				<ReceiptList receipt={headerReceipt} />
-			)}
+			{title === 'Your Receipt!' ? <ReceiptBody /> : <ReceiptBody />}
 		</div>
 	);
 }
