6ad98665b1de59ad605e43c32f4c962c05127eb3
juan-calle <juancmcalle@gmail.com>
2021-07-05 18:04:14 +0100

    :sparkles: (⚛️ Frontend) Route each item in the …

--- client/src/screens/ReceiptList/ReceiptList.tsx
+++ client/src/screens/ReceiptList/ReceiptList.tsx
@@ -4,15 +4,18 @@ import { getUserReceipts } from '../../services/ServerAPIServices';
 import ReceiptListItem from '../../components/Receipt/ReceiptListItem/ReceiptListItem';
 
 function ReceiptList(user_Id: string): JSX.Element {
+	const route = '/receipt/';
 	const receipts = getUserReceipts('user7').map((el) => (
-		<ReceiptListItem
-			key={el.id}
-			logo={el.store.logo}
-			merchantName={el.store.name}
-			timeOfPurchase={el.timeOfPurchase}
-			category={el.category.logotype}
-			total={el.total}
-		/>
+		<a href={route + el.id}>
+			<ReceiptListItem
+				key={el.id}
+				logo={el.store.logo}
+				merchantName={el.store.name}
+				timeOfPurchase={el.timeOfPurchase}
+				category={el.category.logotype}
+				total={el.total}
+			/>
+		</a>
 	));
 
 	return (
