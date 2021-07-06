e1539fbf2e27afe5877a986784c772b39e5cd4e6
juan-calle <juancmcalle@gmail.com>
2021-07-06 01:34:44 +0100

    :sparkles: (⚛️ Frontend) Add an animated dynamic search bar

--- client/src/screens/ReceiptList/ReceiptList.tsx
+++ client/src/screens/ReceiptList/ReceiptList.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ReceiptList.css';
 import { getUserReceipts } from '../../services/ServerAPIServices';
 import MainContainer from '../../components/Main/MainContainer';
 import ReceiptListItem from '../../components/Receipt/ReceiptListItem/ReceiptListItem';
 
 function ReceiptList(user_Id: string): JSX.Element {
+	const [unfilteredData, setUnfilteredData] = useState([]);
+	const [filteredData, setFilteredData] = useState(unfilteredData);
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	const showSearchbar = () => {
+		if (isVisible === false) setIsVisible(true);
+		else setIsVisible(false);
+	};
+
+	const handleSearch = (event) => {
+		const { value } = event.target;
+		let result = [];
+		console.log(value);
+		result = unfilteredData.filter(
+			(data) => data.store.name.toLowerCase().search(value) != -1
+		);
+		setFilteredData(result);
+	};
+
+	useEffect(() => {
+		const allReceipts = getUserReceipts('user7');
+		setUnfilteredData(allReceipts);
+		setFilteredData(allReceipts);
+	}, []);
+
 	const route = '/receipt/';
-	const receipts = getUserReceipts('user7').map((el) => (
+	const receipts = filteredData.map((el) => (
 		<a href={route + el.id}>
 			<ReceiptListItem
 				key={el.id}
@@ -20,12 +45,33 @@ function ReceiptList(user_Id: string): JSX.Element {
 	));
 
 	return (
-		<MainContainer>
-			<>
-				{receipts}
-				{receipts}
-			</>
-		</MainContainer>
+		<>
+			<div className="searchBar">
+				<div className="searchWrapper">
+					<label htmlFor="search">
+						<a href="#" onClick={() => showSearchbar()}>
+							<img
+								src="/assets/logos/categories/png/cat_1.png"
+								alt="search icon"
+							/>
+						</a>
+					</label>
+					<input
+						className="search input__field input__field--makiko"
+						type="text"
+						name="search"
+						id={isVisible.toString()}
+						onChange={(event) => handleSearch(event)}
+					/>
+				</div>
+			</div>
+			<MainContainer>
+				<>
+					{receipts}
+					{receipts}
+				</>
+			</MainContainer>
+		</>
 	);
 }
 
